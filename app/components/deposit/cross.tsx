82eae9fd9585e17aa317fdf376c4585a69da7406
Daniel <danieladammiller@gmail.com>
2024-08-08 14:01:03 -0700

    much markup done

---
+++ app/components/deposit/cross.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Cross = () => {
+  return (
+    <svg className="cross" width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.33301 1.33334L6.66634 6.66667M6.66634 1.33334L1.33301 6.66667" stroke="white" stroke-opacity="0.3" stroke-width="1.5" stroke-linecap="round"/>
+    </svg>
+  );
+}
+
+export default Cross;
--- app/components/deposit/index.tsx
+++ app/components/deposit/index.tsx
@@ -1,13 +1,36 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useEffect, useState } from 'react';
 import { ethers, utils } from 'ethers';
 import { PublicKey, PublicKeyInitData } from '@solana/web3.js';
-import './styles.css'; 
+import './styles.css';
 import TransferArrow from './transferArrow';
 import {
   DynamicConnectButton,
-  DynamicWidget,
+  useUserWallets,
+  useDynamicContext,
+  Wallet,
 } from "@dynamic-labs/sdk-react-core";
+import Cross from './cross';
+import { createPublicClient, formatEther, http } from 'viem'
+import { mainnet } from 'viem/chains'
+import { getBalance, setBalance } from 'viem/actions';
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
+function truncateWalletAddress(str: string) {
+  if (str.length <= 8) {
+    return str;
+  }
+
+  const firstPart = str.slice(0, 4);
+  const lastPart = str.slice(-4);
+
+  return `${firstPart}...${lastPart}`;
+}
+
 
 const DepositInput = () => {
   const [amount, setAmount] = useState('');
@@ -42,7 +65,38 @@ const DepositInput = () => {
 
 const Deposit = () => {
   const [destinationSolana, setDestinationSolana] = useState('');
-  const [amountEther, setAmountEther] = useState('');
+  const [amountEther, setAmountEther] = useState(0);
+  const [balanceEther, setAmountBalanceEther] = useState(0);
+  const userWallets: Wallet[] = useUserWallets() as Wallet[];
+  const solWallet = userWallets.find(w => w.chain == "SOL");
+  const evmWallet = userWallets.find(w => w.chain == "EVM");
+  const { handleUnlinkWallet, rpcProviders } = useDynamicContext();
+
+
+  useEffect(() => {
+    userWallets.forEach(async (wallet) => {
+      if (!wallet) return;
+
+      const provider = rpcProviders.evmDefaultProvider
+
+      if (!provider) return;
+
+      const balance = await getBalance(client, {
+        //@ts-ignore
+        address: wallet.address,
+        blockTag: 'safe'
+      })
+
+      const balanceAsEther = formatEther(balance);
+      const formattedEtherBalance = balanceAsEther.includes('.') ? balanceAsEther.slice(0, balanceAsEther.indexOf('.') + 5) : balanceAsEther
+      const balanceEther = parseFloat(formattedEtherBalance);
+      setAmountBalanceEther(balanceEther);
+
+      console.log('balance', balanceAsEther);
+    });
+  }, [userWallets]);
+
+  console.log({ userWallets })
 
   const contractAddress = '0x83cB71D80078bf670b3EfeC6AD9E5E6407cD0fd1';
   const abi = [
@@ -100,47 +154,85 @@ const Deposit = () => {
           <span className="inactive">Withdraw</span>
         </div>
         <div className="network-section">
-            <div className="arrow-container">
-              <TransferArrow />
-            </div>
-            <div className="network-box">
-              <div className="network-info">
+          <div className="arrow-container">
+            <TransferArrow />
+          </div>
+          <div className="network-box">
+            <div className="network-info">
+              <div className='network-info-left-section'>
                 <img src="eth.png" alt="Ethereum" />
                 <div className="input-inner-container">
                   <span className="direction">From</span>
                   <span className="name">Ethereum Mainnet</span>
                 </div>
               </div>
+              {evmWallet && <div className="network-info-right-section">
+                <div onClick={() => evmWallet && handleUnlinkWallet(evmWallet.id)} className="disconnect">
+                  <Cross />
+                  <div>Disconnect</div>
+                </div>
+                <div className="wallet-addresss">{truncateWalletAddress(userWallets.find(w => w.chain == "EVM")?.address || '')}</div>
+              </div>
+              }
             </div>
-            
+          </div>
+
           <div className="network-box">
             <div className="network-info">
+              <div className='network-info-left-section'>
                 <img src="eclipse.png" alt="Eclipse" />
                 <div className="input-inner-container">
                   <span className="direction">To</span>
                   <span className="name">Eclipse Mainnet</span>
                 </div>
+              </div>
+              {solWallet && <div className="network-info-right-section">
+                <div onClick={() => solWallet && handleUnlinkWallet(solWallet.id)} className="disconnect">
+                  <Cross />
+                  <div>Disconnect</div>
+                </div>
+                <div className="wallet-addresss">{truncateWalletAddress(solWallet?.address || '')}</div>
+              </div>}
             </div>
           </div>
         </div>
         <div className="amount-input">
-          <input
-            type="number"
-            step="0.000000000000000001"
-            placeholder="0 ETH"
-            value={amountEther}
-            onChange={(e) => setAmountEther(e.target.value)}
-          />
+          <div className="amount-input-left">
+            <input
+              type="number"
+              step="0.01"
+              placeholder="0 ETH"
+              value={amountEther}
+              onChange={(e) => setAmountEther(e.target.value)}
+            />
+            {evmWallet &&
+              <div className="balance-info">
+                Bal  &nbsp;  <span style={{ color: '#fff' }}>{balanceEther + " "} </span> &nbsp; ETH
+              </div>}
+          </div>
+          <div className="amount-input-right">
+            <div className="token-display">
+              <div className="token-icon">
+                <img src="eth.png" alt="ETH Icon" />
+              </div>
+              <div className="token-name">ETH</div>
+            </div>
+            <div className="percentage-buttons">
+              <button onClick={() => setAmountEther(balanceEther * 0.25)} className="percentage-button">25%</button>
+              <button onClick={() => setAmountEther(balanceEther * 0.50)} className="percentage-button">50%</button>
+              <button onClick={() => setAmountEther(balanceEther)} className="percentage-button">Max</button>
+            </div>
+          </div>
         </div>
-        <DynamicConnectButton buttonContainerClassName="submit-button">
-          <span style={{ width: '100%' }}>Connect Wallets</span>
-        </DynamicConnectButton>
-        <div className="estimated-time">
-          <span>Estimated time ~ 5 mins</span>
+          <DynamicConnectButton buttonContainerClassName="submit-button">
+            { !evmWallet && !solWallet ? <span style={{ width: '100%' }}>Connect Wallets</span> : <span style={{ width: '100%' }}>Deposit</span>}
+          </DynamicConnectButton>
+          <div className="estimated-time">
+            <span>Estimated time ~ 5 mins</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+      );
 };
 
-export default Deposit;
+      export default Deposit;
