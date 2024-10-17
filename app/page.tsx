ac959746f9ebb7abaa9d8ba24a3296a76c02210a
hkey0 <81494947+hkey0@users.noreply.github.com>
2024-10-17 16:19:26 +0300

    fix: update page content

--- app/page.tsx
+++ app/page.tsx
@@ -26,10 +26,11 @@ export default function Main() {
               height: "100%"
             }}>
               <Header />
-              
+              { isMobile && <br />} 
               <div className="main-content flex flex-col gap-2 items-center">
                 <Deposit amountEther={amountEther} setAmountEther={setAmountEther} />
               </div>
+              { isMobile && <br />} 
               <Footer />
             </div>
           </SkeletonTheme>
