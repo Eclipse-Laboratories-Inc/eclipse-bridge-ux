70ee2c78b8822659de984f07c9deb943c44feb51
hkey0 <81494947+hkey0@users.noreply.github.com>
2024-10-28 18:53:51 +0300

    fix: update lrt popup

--- app/components/Deposit/LrtPopup.tsx
+++ app/components/Deposit/LrtPopup.tsx
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { Cross } from "../icons";
+
+
+const ArrowUpRight: React.FC = () => {
+  return (
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10.4987 8.75V3.5M10.4987 3.5H5.2487M10.4987 3.5L3.64453 10.3542" stroke="#A1FEA0" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  );
+}
 
 const LrtPopup: React.FC = () => {
   const [lrtVisible, setLrtVisible] = useState<boolean>(false);
@@ -26,12 +36,17 @@ const LrtPopup: React.FC = () => {
               <span className="text-[20px] font-semibold w-[247px] h-[52px] text-left line-[26px]" 
                     style={{lineHeight: "26px"}}>Deposit your LRTs to earn restaked yield
               </span>
-              <span className="text-[14px] font-medium text-left" 
-                    style={{ color: "rgba(161, 254, 160, 1)"}}>Deposit Now
-              </span> 
+              <Link href="/mint-teth">
+                <div className="flex flex-row gap-[8px] items-center">
+                  <span className="text-[14px] font-medium text-left" 
+                        style={{ color: "rgba(161, 254, 160, 1)"}}>Deposit Now
+                  </span> 
+                  <ArrowUpRight />
+                </div>
+              </Link>
             </div>
-            <div>
-              <img src="lrt.svg" alt="LRT image." className="h-[99px]" />
+            <div className="ml-[70px]">
+              <img src="lrt.svg" alt="LRT image." className="h-[115px] max-w-none" />
             </div>
             <div className="circle circle1"></div>
             <div className="circle circle2"></div>
