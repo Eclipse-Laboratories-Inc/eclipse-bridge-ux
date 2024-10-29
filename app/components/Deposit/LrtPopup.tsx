b253d60dc2a4c68838ffd8f0e546b792014a52ce
hkey0 <81494947+hkey0@users.noreply.github.com>
2024-10-29 21:41:20 +0300

    fix: add hover state to the 'deposit now' text

--- app/components/Deposit/LrtPopup.tsx
+++ app/components/Deposit/LrtPopup.tsx
@@ -5,8 +5,11 @@ import { Cross } from "../icons";
 
 const ArrowUpRight: React.FC = () => {
   return (
-    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M10.4987 8.75V3.5M10.4987 3.5H5.2487M10.4987 3.5L3.64453 10.3542" stroke="#A1FEA0" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
+    <svg width="14" height="14" viewBox="0 0 14 14" className="stroke-[#A1FEA0] group-hover:stroke-[#74FF71]" fill="none" xmlns="http://www.w3.org/2000/svg"
+      style={{
+        transition: "color 0.1s var(--ease-out-quad)"
+      }}>
+      <path d="M10.4987 8.75V3.5M10.4987 3.5H5.2487M10.4987 3.5L3.64453 10.3542" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   );
 }
@@ -36,9 +39,12 @@ const LrtPopup: React.FC = () => {
                     style={{lineHeight: "26px"}}>Deposit your LRTs to earn restaked yield
               </span>
               <Link href="https://teth.eclipse.xyz">
-                <div className="flex flex-row gap-[8px] items-center">
-                  <span className="text-[14px] font-medium text-left" 
-                        style={{ color: "rgba(161, 254, 160, 1)"}}>Deposit Now
+                <div className="group flex flex-row gap-[8px] items-center">
+                  <span className="text-[14px] font-medium text-left text-[#a1fea0] group-hover:text-[#74FF71]" 
+                  style={{
+                    transition: "color 0.1s var(--ease-out-quad)"
+                  }}>
+                    Deposit Now
                   </span> 
                   <ArrowUpRight />
                 </div>
@@ -61,3 +67,4 @@ const LrtPopup: React.FC = () => {
 }
 
 export default LrtPopup;
+
