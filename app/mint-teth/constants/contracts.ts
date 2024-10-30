bb76d1ef2bea73de27e04e7344683873f732989e
hkey0 <81494947+hkey0@users.noreply.github.com>
2024-10-30 23:42:15 +0300

    fix: hyperlane bug

--- app/mint-teth/constants/contracts.ts
+++ app/mint-teth/constants/contracts.ts
@@ -1,4 +1,4 @@
 export const tellerAddress = "0x6Ae187EacF40ebd1e571a655dB92A1f47452E0Bf";
 export const boringVaultAddress = "0x19e099B7aEd41FA52718D780dDA74678113C0b32";
 export const accountantAddress = "0x8c1902A5996978F2628558DD93d309F7e3926dfD";
-export const warpRouteContractAddress = "0x56Db310194Afd0Eb6A2eC7Dc463d4c727b48CAE2";
+export const warpRouteContractAddress = "0xFbE0c54d696Fa6c93f35D4CA7D656561816C15F6";
