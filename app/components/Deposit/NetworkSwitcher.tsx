79f6da0dbe693adae5f801ba4d2c83c4f5febc0f
Julien <julien@eclipse.builders>
2024-11-14 17:15:29 -0500

    create loading state when switching networks remove dual source of truth for selected network revert to previous network if switching fails

--- app/components/Deposit/NetworkSwitcher.tsx
+++ app/components/Deposit/NetworkSwitcher.tsx
@@ -1,34 +1,39 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Options, useNetwork } from "@/app/contexts/NetworkContext"; 
-import { Chevron, Ellipse } from "../icons";
+import { useNetwork } from "@/app/contexts/NetworkContext"; 
+import { Chevron, Ellipse, Loading } from "../icons";
 import "./NetworkSwitcher.css";
 import { useWallets } from "@/app/hooks/useWallets";
+import { Options } from '@/lib/networkUtils';
 
 export const NetworkSwitcher: React.FC<{isExtended: boolean}> = ({ isExtended }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { selectedOption, setSelectedOption } = useNetwork();
-  const [localSelected, setLocalSelected] = useState<Options>(Options.Mainnet);
+  const [isSwitching, setIsSwitching] = useState(false);
   const modalRef = useRef<HTMLDivElement | null>(null);
   const { evmWallet } = useWallets();
   const customStyle = isExtended 
     ? {}
     : { marginLeft: "240px", marginTop: "0px", width: "150px" }
 
-  useEffect(() => {
-    const switchChain = async (cid: number) => { 
-      if (evmWallet?.connector.supportsNetworkSwitching()) {
-        try { 
-          await evmWallet?.connector.switchNetwork({ networkChainId: cid });
-          setSelectedOption(localSelected);
-        } catch { 
-        };
-      } else {
-      };
-    }
-    
-    const chainId = (localSelected === Options.Mainnet) ? 1 : 11155111; 
-    switchChain(chainId);
-  }, [localSelected, evmWallet?.connected]);
+
+  const switchChain = async (o: Options) => {
+    const cid = o === Options.Mainnet ? 1 : 11155111
+
+    if (evmWallet?.connector.supportsNetworkSwitching()) {
+      try {
+        setIsSwitching(true)
+        await evmWallet?.connector.switchNetwork({ networkChainId: cid });
+        setSelectedOption(o);
+      } catch { 
+        console.error('Failed to switch network. Please ensure you approve network switch requests in your wallet.')
+      } finally {
+        setIsSwitching(false)
+      }
+    } else {
+      // TODO probably should commmunicate this to user via alert
+    };
+  }
+
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -51,10 +56,15 @@ export const NetworkSwitcher: React.FC<{isExtended: boolean}> = ({ isExtended })
          style={{ width: isExtended ? "187px" : "38px"}}
     >
       <div onClick={() => setIsModalOpen(!isModalOpen)} className="net-switcher flex flex-row items-center justify-between">
-        <div className="flex flex-row">
-          <Ellipse />
-          { isExtended && <span>{localSelected}</span> }
-        </div>
+        
+          {isSwitching ?
+            <Loading style={{color: "rgba(161, 254, 160, 1)"}} loadingClassName="" />  :
+            (<div className="flex flex-row">
+              <Ellipse />
+              {isExtended && <span>{selectedOption}</span> }
+            </div>)
+           }
+
         <Chevron />
       </div>
 
@@ -64,7 +74,7 @@ export const NetworkSwitcher: React.FC<{isExtended: boolean}> = ({ isExtended })
              style={customStyle}
         >
           {Object.values(Options).map((option) => (
-            <div key={option} onClick={() => { setLocalSelected(option); setIsModalOpen(!isModalOpen) }}>
+            <div key={option} onClick={() => { switchChain(option); setIsModalOpen(!isModalOpen) }}>
               {option}
             </div>
           ))}
