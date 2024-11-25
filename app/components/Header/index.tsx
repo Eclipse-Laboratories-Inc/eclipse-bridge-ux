c2e2536bd85175d46c88f53b8a8cee63f3f24957
Luis Perrone <peronif5@gmail.com>
2024-11-25 17:55:53 -0600

    Fix mobile layout

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React from "react";
 import { ProfileAvatar } from "./ProfileAvatar";
@@ -6,11 +6,36 @@ import { ProfileAvatar } from "./ProfileAvatar";
 export const Header: React.FC<{ isExtended: boolean }> = ({ isExtended }) => {
   return (
     <header className="header w-full bg-black text-green-500 flex items-center justify-between border-b border-white border-opacity-10">
-      <div className="ecl-logo flex items-center space-x-2" style={{ width: isExtended ? "215px" : "66px"}}>
-      { isExtended 
-        ? <img src="/wordmark.png" className="desktop-logo" alt="Eclipse Logo" width={183} height={34} style={{ marginLeft: "14px", maxWidth: "none" }}  /> 
-        : <img src="/eclipse-e.png" className="mobile-logo" alt="Eclipse Logo" width={35} height={34}  style={{ marginLeft: "14px", maxWidth: "none" }} />
-      } 
+      <div className="flex items-center space-x-2 flex-none sm:flex-1 !w-auto">
+        {isExtended ? (
+          <div className="pr-2">
+            <img
+              src="/wordmark.png"
+              className="desktop-logo hidden sm:block"
+              alt="Eclipse Logo"
+              width={183}
+              height={34}
+              style={{ marginLeft: "14px", maxWidth: "none" }}
+            />
+            <img
+              src="/eclipse-e.png"
+              className="mobile-logo block sm:hidden"
+              alt="Eclipse Logo"
+              width={35}
+              height={34}
+              style={{ marginLeft: "14px", maxWidth: "none" }}
+            />
+          </div>
+        ) : (
+          <img
+            src="/eclipse-e.png"
+            className="mobile-logo"
+            alt="Eclipse Logo"
+            width={35}
+            height={34}
+            style={{ marginLeft: "14px", maxWidth: "none" }}
+          />
+        )}
       </div>
       <ProfileAvatar />
     </header>
--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,88 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 import Deposit from "./components/Deposit";
-import { Header } from './components/Header';
-import { Sidebar } from './components/Sidebar';
-import { Footer } from './components/Footer';
-import { TosClickwrap } from './components/TosClickwrap';
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Footer } from "./components/Footer";
+import { TosClickwrap } from "./components/TosClickwrap";
 import useEthereumData from "@/lib/ethUtils";
 import { NetworkProvider } from "@/app/contexts/NetworkContext";
-import { useWalletClient } from "./hooks"
-import { EthereumDataContext, WalletClientContext } from "./context"
+import { useWalletClient } from "./hooks";
+import { EthereumDataContext, WalletClientContext } from "./context";
 import { useSidebar } from "@/app/contexts/SidebarContext";
-import { ThirdpartyBridgeModalProvider } from '@/app/components/ThirdpartyBridgeModal/ThirdpartyBridgeModalContext';
-import { TransactionProvider } from './components/TransactionPool';
-import { SkeletonTheme } from 'react-loading-skeleton'
+import { ThirdpartyBridgeModalProvider } from "@/app/components/ThirdpartyBridgeModal/ThirdpartyBridgeModalContext";
+import { TransactionProvider } from "./components/TransactionPool";
+import { SkeletonTheme } from "react-loading-skeleton";
 import { Options } from "@/lib/networkUtils";
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.min.css";
 
 export default function Main() {
   const { isSidebar, setIsSidebar } = useSidebar();
-  const [selectedOption, setSelectedOption] = useState(Options.Mainnet)
+  const [selectedOption, setSelectedOption] = useState(Options.Mainnet);
   const { gasPrice, ethPrice, blockNumber } = useEthereumData(selectedOption);
-  const [amountEther, setAmountEther] = useState<number | string | undefined>(undefined);
+  const [amountEther, setAmountEther] = useState<number | string | undefined>(
+    undefined
+  );
   const walletClient = useWalletClient();
 
   return (
     <EthereumDataContext.Provider value={[gasPrice, ethPrice, blockNumber]}>
-      <NetworkProvider selectedOption={selectedOption} setSelectedOption={setSelectedOption}>
+      <NetworkProvider
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+      >
         <WalletClientContext.Provider value={walletClient}>
           <TransactionProvider>
-          <ThirdpartyBridgeModalProvider>
-            <SkeletonTheme baseColor="#FFFFFF0A" highlightColor="#FFFFFF26">
-              <div className="flex items-center text-white flex flex-col justify-between" id="main-content" style={{
-                background: "black",
-                transition: "filter 300ms var(--ease-out-quad)",
-                height: "100%"
-              }}>
-                <Header isExtended={isSidebar} />
-                <TosClickwrap />
-                
-                <div className="flex flex-row w-full items-center" style={{height: "100%"}}>
-                  <Sidebar isExtended={isSidebar} setIsExtended={setIsSidebar} />
-                  <div className="flex flex-col items-center" style={{ gap: "13px", flexGrow: "1"}}>
-                    <div className="main-content flex flex-col gap-2 items-center">
-                      <Deposit amountEther={amountEther} setAmountEther={setAmountEther} />
-                      <br />
+            <ThirdpartyBridgeModalProvider>
+              <SkeletonTheme baseColor="#FFFFFF0A" highlightColor="#FFFFFF26">
+                <div
+                  className="flex items-center text-white flex flex-col justify-between"
+                  id="main-content"
+                  style={{
+                    background: "black",
+                    transition: "filter 300ms var(--ease-out-quad)",
+                    // height: "100%",
+                  }}
+                >
+                  <Header isExtended={isSidebar} />
+                  <TosClickwrap />
+
+                  <div className="flex flex-row w-full">
+                    <Sidebar
+                      isExtended={isSidebar}
+                      setIsExtended={setIsSidebar}
+                    />
+                    <div
+                      className="flex flex-col items-center"
+                      style={{ gap: "13px", flexGrow: "1" }}
+                    >
+                      <div className="main-content flex flex-col gap-2 items-center">
+                        <Deposit
+                          amountEther={amountEther}
+                          setAmountEther={setAmountEther}
+                        />
+                        <br />
+                      </div>
                     </div>
                   </div>
+                  <Footer />
                 </div>
-                <Footer />
-              </div>
-              <ToastContainer
-                autoClose={3000}
-                closeOnClick={true}
-                hideProgressBar={true}
-                position={'top-center'}
-                toastStyle={{backgroundColor: 'transparent'}}
-                className={'!w-auto'}
-                limit={3}
-              />
-            </SkeletonTheme>
+                <ToastContainer
+                  autoClose={3000}
+                  closeOnClick={true}
+                  hideProgressBar={true}
+                  position={"top-center"}
+                  toastStyle={{ backgroundColor: "transparent" }}
+                  className={"!w-auto"}
+                  limit={3}
+                />
+              </SkeletonTheme>
             </ThirdpartyBridgeModalProvider>
           </TransactionProvider>
-        </WalletClientContext.Provider >
+        </WalletClientContext.Provider>
       </NetworkProvider>
     </EthereumDataContext.Provider>
   );
 }
-
