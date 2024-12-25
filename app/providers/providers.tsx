8330f44ba27844e04a0fb63d6568b5c793aca5b8
hkey0 <81494947+hkey0@users.noreply.github.com>
2024-12-25 06:32:14 +0300

    fix: use url parameters to set active tab

--- app/providers/providers.tsx
+++ app/providers/providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, type ReactNode } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { SidebarProvider } from "@/app/contexts/SidebarContext";
 import { RelayKitProvider } from "@/app/providers/RelayKitProvider";
 import { RelayChain } from "@reservoir0x/relay-sdk";
@@ -17,7 +18,9 @@ export function Providers(props: {
     <RelayKitProvider chains={props.chains}>
       <QueryClientProvider client={queryClient}>
         <WalletFilterProvider>
-          <SidebarProvider>{props.children}</SidebarProvider>
+          <BrowserRouter>
+            <SidebarProvider>{props.children}</SidebarProvider>
+          </BrowserRouter>
         </WalletFilterProvider>
       </QueryClientProvider>
     </RelayKitProvider>
