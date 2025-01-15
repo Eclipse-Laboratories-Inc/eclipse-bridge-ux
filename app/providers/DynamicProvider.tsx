f6ea1087f04a1148c6b75845831dacfadbf71787
hkey <81494947+hkey0@users.noreply.github.com>
2025-01-15 05:13:56 +0300

    add wallet filter

--- app/providers/DynamicProvider.tsx
+++ app/providers/DynamicProvider.tsx
@@ -161,6 +161,14 @@ export const DynamicProvider = (props: {
             mainContent.style.filter = "";
             setWalletFilter(undefined);
           },
+          walletsFilter: walletFilter
+            ? FilterChain(walletFilter)
+            : (wallets) =>
+              wallets.filter(
+                (w) =>
+                  w.walletConnector.supportedChains.includes("EVM") ||
+                  eclipseWallets.includes(w.key),
+          ),
           onWalletAdded: (args) => {
             if (args.wallet.key === "backpacksol") {
               //@ts-ignore
