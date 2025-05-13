eeee466daa02eca5da1e56c0a4c5864a757f4d21
hkey0 <81494947+hkey0@users.noreply.github.com>
2025-05-13 22:09:51 +0300

    create mint tusd page

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -10,61 +10,62 @@ import { composeEclipsescanUrl, useNetwork } from "@/app/contexts/NetworkContext
 
 const ToggleIcon: React.FC<{ isExtended: boolean }> = ({ isExtended }) => {
   return (
-    <svg width="20" 
-         height="20" 
-         viewBox="0 0 20 20" 
-         fill="none" 
-         xmlns="http://www.w3.org/2000/svg" 
-         style={ !isExtended ? { transform: "rotate(180deg)", transition: "transform 0.1s" } : { transition: "transform 0.1s" }}>
-      <path d="M8.33509 13.3333L5.88564 10.8839C5.39749 10.3957 5.39749 9.60421 5.88564 9.11604L8.33509 6.66663M14.1685 13.3333L11.719 10.8839C11.2308 10.3957 11.2308 9.60421 11.719 9.11604L14.1685 6.66663" stroke="white" stroke-opacity="0.3" stroke-width="1.7971" stroke-linecap="round" stroke-linejoin="round"/>
+    <svg width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={!isExtended ? { transform: "rotate(180deg)", transition: "transform 0.1s" } : { transition: "transform 0.1s" }}>
+      <path d="M8.33509 13.3333L5.88564 10.8839C5.39749 10.3957 5.39749 9.60421 5.88564 9.11604L8.33509 6.66663M14.1685 13.3333L11.719 10.8839C11.2308 10.3957 11.2308 9.60421 11.719 9.11604L14.1685 6.66663" stroke="white" stroke-opacity="0.3" stroke-width="1.7971" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
   );
 }
 
-const SidebarItem: React.FC<{name: string, icon: ReactNode, isExtended: boolean}> = ({ name, icon, isExtended }) => {  
+const SidebarItem: React.FC<{ name: string, icon: ReactNode, isExtended: boolean }> = ({ name, icon, isExtended }) => {
   const { selectedOption } = useNetwork()
   const [hover, setHover] = useState(false);
   const pathName = usePathname();
   let targetHref = toKebabCase(name)
   if (targetHref === "eclipsescan") { targetHref = composeEclipsescanUrl(selectedOption) }
-  if (targetHref === "ecosystem")   { targetHref = "https://www.eclipse.xyz/ecosystem" }
+  if (targetHref === "ecosystem") { targetHref = "https://www.eclipse.xyz/ecosystem" }
 
   return (
     <Link href={targetHref} target={targetHref.includes("https") ? "_blank" : ""}>
-    <div className={ `side-item flex flex-row items-center ${ pathName.slice(1) === toKebabCase(name) ? "highlight-icon" : "" }`} 
-         style={{ gap: "11px", padding: isExtended ? "4px" : "0px", background: !isExtended ? "none" : ""}}
-         onMouseEnter={() => setHover(true)}
-         onMouseLeave={() => setHover(false)}
-         >
-      <div className={ `flex items-center justify-center side-icon-box` } style={{width: "38px", height: "38px"}}>
-        {icon}
+      <div className={`side-item flex flex-row items-center ${pathName.slice(1) === toKebabCase(name) ? "highlight-icon" : ""}`}
+        style={{ gap: "11px", padding: isExtended ? "4px" : "0px", background: !isExtended ? "none" : "" }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className={`flex items-center justify-center side-icon-box`} style={{ width: "38px", height: "38px" }}>
+          {icon}
+        </div>
+        {isExtended && <span style={{ fontWeight: "500", fontSize: "16px" }}>{name}</span>}
+
+        {hover && !isExtended && (
+          <>
+            <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginLeft: "-7px" }}>
+              <path style={{ stroke: "none" }} d="M5 7.5V0.5L1.17033 3.18077C0.601614 3.57887 0.601616 4.42113 1.17033 4.81923L5 7.5Z" fill="white" fill-opacity="0.08" />
+            </svg>
+            <div className="sidebar-hover flex items-center justify-center">
+              <span>{name}</span>
+            </div>
+          </>
+        )}
       </div>
-      { isExtended && <span style={{fontWeight: "500", fontSize: "16px"}}>{name}</span> } 
-      
-      { hover && !isExtended && (
-        <>
-          <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ marginLeft: "-7px"}}>
-            <path style={{ stroke: "none" }} d="M5 7.5V0.5L1.17033 3.18077C0.601614 3.57887 0.601616 4.42113 1.17033 4.81923L5 7.5Z" fill="white" fill-opacity="0.08"/>
-          </svg>
-          <div className="sidebar-hover flex items-center justify-center">
-            <span>{ name }</span>
-          </div>
-        </>
-      )}
-    </div>
     </Link>
   );
 }
 
-export const Sidebar: React.FC<{ isExtended: boolean, setIsExtended: React.Dispatch<React.SetStateAction<boolean>>}> = ({ isExtended, setIsExtended }) => {
+export const Sidebar: React.FC<{ isExtended: boolean, setIsExtended: React.Dispatch<React.SetStateAction<boolean>> }> = ({ isExtended, setIsExtended }) => {
   return (
-    <div className="sidebar flex flex-col justify-between sm:flex hidden sm:block" style={{ width: isExtended ? "215px" : "66px"}}>
+    <div className="sidebar flex flex-col justify-between sm:flex hidden sm:block" style={{ width: isExtended ? "215px" : "66px" }}>
       <div>
         <NetworkSwitcher isExtended={isExtended} />
         <div className="sidebar-tabs flex flex-col" style={{ marginTop: "14px", marginLeft: "14px" }}>
           <SidebarItem isExtended={isExtended} name="Bridge" icon={<BridgeIcon />} />
           <SidebarItem isExtended={isExtended} name="Gas Station" icon={<GasStationIcon />} />
           <SidebarItem isExtended={isExtended} name="Mint tETH" icon={<TethIcon />} />
+          <SidebarItem isExtended={isExtended} name="Mint tUSD" icon={<TethIcon />} />
           <SidebarItem isExtended={isExtended} name="Eclipsescan" icon={<ScanIcon />} />
           <SidebarItem isExtended={isExtended} name="Ecosystem" icon={<EcosystemIcon />} />
         </div>
@@ -74,8 +75,8 @@ export const Sidebar: React.FC<{ isExtended: boolean, setIsExtended: React.Dispa
             justify-center ml-[calc(100%-15.5px)] transition-all 
             close-sidebar-icon
       "
-           onClick={() => {setIsExtended((current) => !current)}}>
-        <ToggleIcon isExtended={ isExtended }/>
+        onClick={() => { setIsExtended((current) => !current) }}>
+        <ToggleIcon isExtended={isExtended} />
       </div>
     </div>
   );
