a1e9e01b5c45b4fb2a69cba1acd29d23cc3154ce
Frog <frostfrog2@gmail.com>
2025-07-07 10:51:50 +0800

    update testnet address and the rpc url

--- app/contexts/NetworkContext.tsx
+++ app/contexts/NetworkContext.tsx
@@ -65,9 +65,9 @@ interface NetworkProviderProps extends PropsWithChildren {
 export const NetworkProvider = ({ selectedOption, setSelectedOption, children }: NetworkProviderProps) => {
   const isMainnet = (selectedOption === Options.Mainnet);
   const bridgeProgram   = isMainnet ? "br1xwubggTiEZ6b7iNZUwfA3psygFfaXGfZ1heaN9AW" : "br1t2MBNdtVRZk3taADwNLt142cVNkekXe1hn3qJVYb"
-  const eclipseRpc      = isMainnet ? "https://eclipse.lgns.net/" : "https://testnet-archive-rpc.dev.eclipsenetwork.xyz/"
-  // TODO: We will need to update this with the v2 addresses once we have them 
-  const contractAddress = isMainnet ? "0x2B08D7cF7EafF0f5f6623d9fB09b080726D4be11" : "0xe49aaa25a10fd6e15dd7ddcb50904ca1e91f6e01"
+  const eclipseRpc      = isMainnet ? "https://eclipse.lgns.net/" : "https://testnet.dev2.eclipsenetwork.xyz"
+  // TODO: We will need to update this with the v2 addresses once we have them
+  const contractAddress = isMainnet ? "0x2B08D7cF7EafF0f5f6623d9fB09b080726D4be11" : "0x568a5e1Ad8F6FA834C0d28c9D29Fb3eB86fe84E9"
   const legacyAddress   = isMainnet ? "0x2B08D7cF7EafF0f5f6623d9fB09b080726D4be11" : "0xe49aaa25a10fd6e15dd7ddcb50904ca1e91f6e01" // V1 contract address for backward compatibility
   const relayerAddress  = isMainnet ? "CrfbABN2sSvmoZLu9eDDfXpaC2nHg42R7AXbHs9eg4S9" : "ec1vCnQKsQSnTbcTyc3SH2azcDXZquiFB3QqtRvm3Px"
   const configAccount   = isMainnet ? "B6UA9rd6Qrx9chsrcMWPV3EFnSb1cbnf7AA2wdkhkpqw" : "A3jHKVwNvrvTjnUPGKYei9jbPn7NcraD6H94ewWyfVMY"
