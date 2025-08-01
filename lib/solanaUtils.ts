1e2c4fd8e4c6f1b6f4fd4661d2b2cd0a17a3fd75
hkey <81494947+hkey0@users.noreply.github.com>
2025-08-01 04:49:30 +0300

    fix rpc usage in gas station util functions (#421)

--- lib/solanaUtils.ts
+++ lib/solanaUtils.ts
@@ -1,43 +1,47 @@
-const solanaWeb3 = require('@solana/web3.js');
-import { PublicKey, PublicKeyInitData, Connection } from '@solana/web3.js';
-import { createAssociatedTokenAccount, getAccount, getAssociatedTokenAddress, TokenAccountNotFoundError, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
-import { toHex } from 'viem';
-import { toTokenAmount } from '@orca-so/whirlpools-sdk';
+const solanaWeb3 = require("@solana/web3.js");
+import { PublicKey, PublicKeyInitData, Connection } from "@solana/web3.js";
+import {
+  getAssociatedTokenAddress,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
+import { toHex } from "viem";
 
 export async function getWalletBalance(publicKey: String, eclipseRpc: string) {
   // Connect to the Solana mainnet
-  const connection = new solanaWeb3.Connection(
-    eclipseRpc,
-    'confirmed'
-  );
+  const connection = new solanaWeb3.Connection(eclipseRpc, "confirmed");
 
   // Fetch the balance
-  const balance = await connection.getBalance(new solanaWeb3.PublicKey(publicKey));
+  const balance = await connection.getBalance(
+    new solanaWeb3.PublicKey(publicKey),
+  );
 
   // Convert balance from lamports to SOL (1 SOL = 10^9 lamports)
   return balance / solanaWeb3.LAMPORTS_PER_SOL;
 }
 
 export async function getTokenBalance(tokenMint: string, wallet: string) {
-  const connection = new Connection("https://eclipse.helius-rpc.com", "finalized");
+  const eclipseRpcUrl = process.env.NEXT_PUBLIC_ECLIPSE_RPC;
+  const connection = new Connection(eclipseRpcUrl ?? "", "finalized");
 
   const mintAddress = new PublicKey(tokenMint);
   const walletAddress = new PublicKey(wallet);
 
-  console.log(TOKEN_2022_PROGRAM_ID)
+  console.log(TOKEN_2022_PROGRAM_ID);
   const associatedTokenAddress = await getAssociatedTokenAddress(
     mintAddress,
     walletAddress,
     false,
-    TOKEN_2022_PROGRAM_ID
+    TOKEN_2022_PROGRAM_ID,
   );
   try {
-    const tokenAccount = await connection.getTokenAccountBalance(associatedTokenAddress);
+    const tokenAccount = await connection.getTokenAccountBalance(
+      associatedTokenAddress,
+    );
     const tokenBalance = tokenAccount.value;
 
-  return parseInt(tokenBalance.amount);
+    return parseInt(tokenBalance.amount);
   } catch {
-    return 0
+    return 0;
   }
 }
 
@@ -46,8 +50,7 @@ export const solanaToBytes32 = (solanaAddress: PublicKeyInitData) => {
     const publicKey = new PublicKey(solanaAddress);
     return toHex(publicKey.toBytes().slice(0, 32));
   } catch (error) {
-      console.error('Invalid Solana address', error);
-      throw new Error('Invalid Solana address');
-    }
- };
-
+    console.error("Invalid Solana address", error);
+    throw new Error("Invalid Solana address");
+  }
+};
