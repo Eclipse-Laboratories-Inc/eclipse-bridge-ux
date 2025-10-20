0e9db7211a6c41896174aff6789dcdeb248236d4
dlin128 <david@eclipse.builders>
2025-10-20 16:45:56 -0700

    add todo comment

--- app/components/WithdrawDetails/index.tsx
+++ app/components/WithdrawDetails/index.tsx
@@ -204,6 +204,7 @@ export const WithdrawDetails: React.FC<TransactionDetailsProps> = ({
     // Use the contract address from the withdrawal data (V2 API provides this)
     let targetContractAddress = tx[0].bridge;
     // For testnet, use the contract address from the network context since we have upgraded to v3 contract and it will route the claim to the v2 and v1 contracts
+    // TODO: Remove this once we have upgraded to v3 contract on mainnet
     if (!isMainnet) {
       targetContractAddress = contractAddress;
     }
